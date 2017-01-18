9b08568d3ea5e6ed432b149d9183acd2425c2a80
Brendan Gallagher <bgallag6@gmu.edu>
2017-01-17 19:16:03 -0500

    specified docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ sys.path.insert(0, os.path.abspath('./py_modules'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-#    'numpydoc' 
+    'numpydoc' 
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -126,6 +126,9 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
