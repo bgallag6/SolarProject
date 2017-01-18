edb246b8f159548b1526538e46431be54a15db18
Brendan Gallagher <bgallag6@gmu.edu>
2017-01-17 19:05:44 -0500

    docs work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,8 @@ sys.path.insert(0, os.path.abspath('./py_modules'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'numpydoc' ]
+#    'numpydoc' 
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
